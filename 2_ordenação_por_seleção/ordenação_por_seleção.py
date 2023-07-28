5d218996304f92252751eaaf6cf03db06e955b17
lucaslinyker <lucaslinyker@outlook.com>
2023-07-28 19:46:58 -0300

    Update ordenação_por_seleção.py

--- 2_ordenação_por_seleção/ordenação_por_seleção.py
+++ 2_ordenação_por_seleção/ordenação_por_seleção.py
@@ -1,17 +1,17 @@
 def buscaMenor(arr):
     menor = arr[0]
-    menor_indice = 0
+    menor_índice = 0
     for i in range(1, len(arr)):
         if arr[i] < menor:
             menor = arr[i]
-            menor_indice = i
-    return menor_indice
+            menor_índice = i
+    return menor_índice
 
-def ordenacaoporSelecao(arr):
+def ordenaçãoPorSeleção(arr):
     novoArr = []
     for i in range(len(arr)):
         menor = buscaMenor(arr)
         novoArr.append(arr.pop(menor))
     return novoArr
 
-print(ordenacaoporSelecao([5, 3, 6, 2, 10]))
+print(ordenaçãoPorSeleção([5, 3, 6, 2, 10]))
