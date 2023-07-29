8d6e630bcf1fd88e9233a5a962f11b3fc3c1dbac
lucaslinyker <lucaslinyker@outlook.com>
2023-07-29 11:48:34 -0300

    Update ordenação_por_seleção.py

--- 2_ordenação_por_seleção/ordenação_por_seleção.py
+++ 2_ordenação_por_seleção/ordenação_por_seleção.py
@@ -1,17 +1,23 @@
 def buscaMenor(arr):
-    menor = arr[0]
-    menor_índice = 0
+    menor = arr[0] #1
+    menor_índice = 0 #2
     for i in range(1, len(arr)):
         if arr[i] < menor:
             menor = arr[i]
             menor_índice = i
     return menor_índice
 
-def ordenaçãoPorSeleção(arr):
+#1 Armazena o menor valor.
+#2 Armazena o índice do menor valor.
+
+def ordenaçãoPorSeleção(arr): #1
     novoArr = []
     for i in range(len(arr)):
-        menor = buscaMenor(arr)
+        menor = buscaMenor(arr) #2
         novoArr.append(arr.pop(menor))
     return novoArr
 
 print(ordenaçãoPorSeleção([5, 3, 6, 2, 10]))
+
+#1 Ordenações em um array.
+#2 Encontra o menor elemento do array e adiciona ao novo array.
